c0e0f7c0a688d03eff7ac4d11615b1cfdf1dd3ab
Italo Fernandes <italo.feernandes@gmail.com>
2022-03-20 13:57:11 -0300

    wip in application

--- src/main/java/dev/italofernandes/account/application/adapter/AccountAdapter.java
+++ src/main/java/dev/italofernandes/account/application/adapter/AccountAdapter.java
@@ -1,5 +1,12 @@
 package dev.italofernandes.account.application.adapter;
 
+import dev.italofernandes.account.application.dto.response.AccountBalanceResponse;
+import dev.italofernandes.account.domain.models.Account;
+
 public class AccountAdapter {
+
+    public static AccountBalanceResponse toDtoBalance(Account account) {
+        return new AccountBalanceResponse(account.getId(), account.getBalance());
+    }
 
 }
