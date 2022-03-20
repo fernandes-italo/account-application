8ec3efd235d317f8412979992ef52b9012415d22
Italo Fernandes <italo.feernandes@gmail.com>
2022-03-20 19:26:08 -0300

    add debit method in the controller

--- src/main/java/dev/italofernandes/account/api/AccountController.java
+++ src/main/java/dev/italofernandes/account/api/AccountController.java
@@ -3,11 +3,15 @@ package dev.italofernandes.account.api;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.italofernandes.account.application.AccountApplication;
+import dev.italofernandes.account.application.dto.request.AccountDebitRequest;
 import dev.italofernandes.account.application.dto.response.AccountBalanceResponse;
+import dev.italofernandes.account.application.dto.response.AccountDebitResponse;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -23,6 +27,16 @@ public class AccountController {
         AccountBalanceResponse accountBalanceResponse = accountApplication.getBalance(accountId);
 
         return ResponseEntity.ok(accountBalanceResponse);
+    }
+
+    @PostMapping("/debit/{accountId}")
+    public ResponseEntity<AccountDebitResponse> debit(@PathVariable Long accountId,
+            @RequestBody AccountDebitRequest accountDebitRequest) {
+
+        AccountDebitResponse accountDebitResponse = accountApplication.debit(accountId, accountDebitRequest);
+
+        return ResponseEntity.ok(accountDebitResponse);
+
     }
 
 }
